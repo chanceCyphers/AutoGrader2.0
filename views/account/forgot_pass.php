42bfa59c7b43767f7c8ea59f890f3e3e12860e30
Brandon Bush <brandontbush@gmail.com>
2015-10-31 07:15:15 -0400

    Add account related view pages.
    Registration, Account, and Forgot Password view templates created.

---
+++ views/account/forgot_pass.php
@@ -0,0 +1,9 @@
+<?php
+	# Copy from original auto-grader and apply
+	# System auto generates password after verifying user credentials and then emails.
+	
+
+
+
+
+?>
---
+++ views/account/registration.php
@@ -0,0 +1,19 @@
+<div id="registerDiv">
+    <h2>Enter New Account Information</h2>
+    <form action="  " method="   ">
+        <div>
+            <p>Desired Username:</p>
+            <input type="text" name="username" size="30">
+        </div>
+        <div>
+            <p>Email Address:</p>
+            <input type="text" name="email" size="40">
+        </div>
+        <div>
+            <input type="submit" value="submit">
+        </div>
+        <div>
+            <p>New user accounts will have a password emailed to them within 24 hours.</p>
+        </div>
+    </form>
+</div>
