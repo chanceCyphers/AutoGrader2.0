455b7aa4d46b0fdb59324752ae899b469e7f7e9c
Brandon Bush <brandontbush@gmail.com>
2015-11-27 10:31:34 -0500

    Continued work on Admin section

--- models/admin_manager.php
+++ models/admin_manager.php
@@ -18,7 +18,35 @@
 
 		}
 
+		# Obtains a list of all of the group names
+		public static function getAllGroups() {
+			$db = Db::getInstance();
 
+			$groupQuery = $db->prepare('SELECT * FROM groups');
+			$groupQuery->execute();
+			$groupList = $groupQuery->fetch(PDO::FETCH_ASSOC);
+
+			return $groupList;
+		}
+
+		# Obtains a list of all of the permission names
+		public static function getAllPermissions() {
+			$db = Db::getInstance();
+
+			$permQuery = $db->prepare('SELECT * FROM permissions');
+			$permQuery->execute();
+			$permList = $permQuery->fetch(PDO::FETCH_ASSOC);
+
+			return $permList;
+		}
+
+		public static function setUserGroup($group_id) {
+
+		}
+
+		public static function setUserPermission($perm_id) {
+			
+		}
 
 	}
 
--- views/admin/changeInfo.php
+++ views/admin/changeInfo.php
@@ -1,6 +1,21 @@
 <?php
 
-	echo "<h2>" . $userProfile['username'];
+	echo "<h2>" . $userProfile['username'] . "</h2><br/>";
+	echo "<p>" . $userProfile['email'] . "</p><br/>";
 
+	echo "<form action='?controller=admin&action=changeInfo' method='post'>";
+	echo "<p> Current Group Association: ";
+	echo $userProfile["group_des"] . "</p><br/>";
+	echo "Change Group To: ";
+	# DROPDOWN MENU HERE
+	echo "<input type='submit' value='Change' />";
+	echo "</form>";
 
+	echo "<form action='?controller=admin&action=changeInfo' method='post'>";
+	echo "<p> Current Permissions: ";
+	echo $userProfile["perm_des"] . "</p><br/>";
+	echo "Change Permissions To: ";
+	#DROPDOWN MENU HERE
+	echo "<input type='submit' value='Change' />";
+	echo "</form>";
 ?>
