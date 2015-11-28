bf6bc2e4ef9197cc550e4efacc49a78a149215f5
Brandon Bush <brandontbush@gmail.com>
2015-11-28 13:25:53 -0500

    Admin functionality is in place.

--- controllers/admin_controller.php
+++ controllers/admin_controller.php
@@ -16,10 +16,28 @@
 			if (isset($_POST['username'])) {
 				$userToChange = $_POST['username'];
 				$userProfile = AdminManager::searchUser($userToChange);
+				$groupList = AdminManager::getAllGroups();
+				$permList = AdminManager::getAllPermissions();
 				require_once("views/admin/changeInfo.php");
 			}
+		}
 
+		public function setUserGroup() {
+			if (isset($_POST['userGroup'])) {
+				$username = $_POST['username'];
+				$newGroup = $_POST['userGroup'];
+				AdminManager::setUserGroup($username, $newGroup);
+				echo "User edited successfully";
+			}
+		}
 
+		public function setUserPermission() {
+			if (isset($_POST['userPermission'])) {
+				$username = $_POST['username'];
+				$newPermission = $_POST['userPermission'];
+				AdminManager::setUserPermission($username, $newPermission);
+				echo "User edited successfully";
+			}
 		}
 
 	}
